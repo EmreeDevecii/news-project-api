0f33b73c7c77755f4a3e8b67535e8a8f5460829f
Emre Deveci <144159254+EmreeDevecii@users.noreply.github.com>
2024-09-26 00:22:10 +0300

    Models added

--- core/news/admin.py
+++ core/news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from news.models import Makale, Yorum, Favori
 
 # Register your models here.
+
+admin.site.register(Makale)
+admin.site.register(Yorum)
+admin.site.register(Favori)
--- core/news/models.py
+++ core/news/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Makale(models.Model):
+    baslik = models.CharField(max_length=255)
+    yazar = models.CharField(max_length=255)
+    icerik = models.TextField(blank=True, null=True)
+    aktif = models.BooleanField(default=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    güncellenme_tarihi = models.DateTimeField(auto_now=True)
+    yayin_tarihi = models.DateTimeField()
+
+    makale_resmi = models.ImageField(upload_to='makale_resimleri/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.baslik} - {self.yazar}'
+    
+    class Meta:
+        verbose_name_plural = "Makaleler"
+    
+
+class Yorum(models.Model):
+    makale = models.ForeignKey(Makale, on_delete=models.CASCADE, related_name='yorumlar')
+    yorum_sahibi = models.ForeignKey(User, on_delete=models.CASCADE, related_name='kullanici_yorumlari')
+    yorum = models.TextField(blank=True, null=True)
+    aktif = models.BooleanField(default=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    güncellenme_tarihi = models.DateTimeField(auto_now=True)
+    degerlendirme = models.PositiveIntegerField(
+        validators= [MinValueValidator(1), MaxValueValidator(5)],
+    )
+
+    def __str__(self):
+        return f'{self.makale} - {self.yorum_sahibi} - {self.degerlendirme}'
+    
+    class Meta:
+        verbose_name_plural = "Yorumlar"
+    
+class Favori(models.Model):
+    favori_sahibi = models.ForeignKey(User, on_delete=models.CASCADE, related_name='kullanici_favorileri')
+    makale = models.ForeignKey(Makale, on_delete=models.CASCADE, related_name='favoriler')
+
+    def __str__(self):
+        return f'{self.makale} - {self.favori_sahibi}'
+    
+    class Meta:
+        verbose_name_plural = "Favoriler"
